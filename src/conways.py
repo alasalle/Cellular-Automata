01757e647ca6889f04e792a5619ab7252f763165
alasalle <angela.lasalle91@gmail.com>
2019-08-15 23:53:04 -0400

    MVP complete

--- src/conways.py
+++ src/conways.py
@@ -8,23 +8,41 @@ SILVER = (192,192,192)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
-WIN_SIZE = 600
-CELL_SIZE = 25
+
+WIN_SIZE = 500
+CELL_SIZE = 20
 MARGIN = 5
 
-world = np.random.choice(a=[0, 1], size=(WIN_SIZE // CELL_SIZE + MARGIN, WIN_SIZE // CELL_SIZE + MARGIN))
-
+world = np.random.choice(a=[0, 1], size=(WIN_SIZE// CELL_SIZE + MARGIN, WIN_SIZE// CELL_SIZE + MARGIN))
+currentWorld = world
+generation = 1
+fps = 1
 pygame.init()
  
 # Set the width and height of the screen [width, height]
-size = (WIN_SIZE, WIN_SIZE)
+size = (520, 655)
 screen = pygame.display.set_mode(size)
 
+fasterButton = pygame.Rect(100, 100, 123.75, 50)
+slowerButton = pygame.Rect(100, 100, 123.75, 50)
+togglePlayButton = pygame.Rect(100, 100, 123.75, 50)
+restartButton = pygame.Rect(100, 100, 123.75, 50)
+
+fasterButton.x = 5
+fasterButton.y = 600
+slowerButton.x = 133.75
+slowerButton.y = 600
+togglePlayButton.x = 262.5
+togglePlayButton.y = 600
+restartButton.x = 391.25
+restartButton.y = 600
+
 # Add a title
-pygame.display.set_caption("Conway's Game of Life")
+pygame.display.set_caption('Conway\'s Game of Life Generation')
  
 # Loop until the user clicks the close button.
 done = False
+state = 'RUNNING'
  
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
@@ -35,6 +53,22 @@ while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+        if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = event.pos
+
+                if fasterButton.collidepoint(mouse_pos):
+                    fps += 1
+                if slowerButton.collidepoint(mouse_pos):
+                    if fps > 1:
+                        fps -= 1
+                if togglePlayButton.collidepoint(mouse_pos):
+                    if state == 'RUNNING':
+                        state = 'PAUSED'
+                    elif state == 'PAUSED':
+                        state = 'RUNNING'
+                if restartButton.collidepoint(mouse_pos):
+                    world = np.random.choice(a=[0, 1], size=(WIN_SIZE// CELL_SIZE + MARGIN, WIN_SIZE// CELL_SIZE + MARGIN))
+                    generation = 0
  
     # --- Game logic should go here
     def cell_next_gen(x, y, world):
@@ -53,25 +87,108 @@ while not done:
     screen.fill(WHITE)
  
     # --- Drawing code should go here
-    newWorld = np.copy(world)
-    for (x, y), value in np.ndenumerate(world):
-        newWorld[x, y] = cell_next_gen(x, y, world)
-        if newWorld[x, y] == 1:
-            pygame.draw.rect(screen, WHITE, (CELL_SIZE * (x - 1), CELL_SIZE * (y - 1), CELL_SIZE, CELL_SIZE), MARGIN)
-            pygame.draw.rect(screen, GREEN, (CELL_SIZE * (x - 1), CELL_SIZE * (y - 1), CELL_SIZE, CELL_SIZE), 0)
+    if state == 'RUNNING':
 
-        else:
-            pygame.draw.rect(screen, WHITE, (CELL_SIZE * (x - 1), CELL_SIZE * (y - 1), CELL_SIZE, CELL_SIZE), MARGIN)
-            pygame.draw.rect(screen, SILVER, (CELL_SIZE * (x - 1), CELL_SIZE * (y - 1), CELL_SIZE, CELL_SIZE), 0)
+        newWorld = np.copy(world)
+        for (x, y), value in np.ndenumerate(world):
+            newWorld[x, y] = cell_next_gen(x, y, world)
+            if newWorld[x, y] == 1:
+                pygame.draw.rect(screen, WHITE, (CELL_SIZE * (x - 1), CELL_SIZE * (y - 1), CELL_SIZE, CELL_SIZE), MARGIN)
+                pygame.draw.rect(screen, GREEN, (CELL_SIZE * (x - 1), CELL_SIZE * (y - 1), CELL_SIZE, CELL_SIZE), 0)
+
+            else:
+                pygame.draw.rect(screen, WHITE, (CELL_SIZE * (x - 1), CELL_SIZE * (y - 1), CELL_SIZE, CELL_SIZE), MARGIN)
+                pygame.draw.rect(screen, SILVER, (CELL_SIZE * (x - 1), CELL_SIZE * (y - 1), CELL_SIZE, CELL_SIZE), 0)
+
+            currentWorld = newWorld
+        
+        pygame.draw.rect(screen, GREEN, fasterButton)
+        pygame.draw.rect(screen, GREEN, slowerButton)
+        pygame.draw.rect(screen, GREEN, togglePlayButton)
+        pygame.draw.rect(screen, GREEN, restartButton)
+
+        font = pygame.font.Font(pygame.font.get_default_font(), 20)
+        fasterButtonText = font.render("FASTER", True, BLACK)
+        slowerButtonText = font.render("SLOWER", True, BLACK)
+        togglePlayButtonText = font.render("PLAY/PAUSE", True, BLACK)
+        restartButtonText = font.render("RESTART", True, BLACK)
+
+        fasterText_rect = fasterButtonText.get_rect()
+        fasterText_rect.center = fasterButton.center
+        screen.blit(fasterButtonText, fasterText_rect)
+
+        slowerText_rect = slowerButtonText.get_rect()
+        slowerText_rect.center = slowerButton.center
+        screen.blit(slowerButtonText, slowerText_rect)
+
+        togglePlayText_rect = togglePlayButtonText.get_rect()
+        togglePlayText_rect.center = togglePlayButton.center
+        screen.blit(togglePlayButtonText, togglePlayText_rect)
+
+        restartText_rect = restartButtonText.get_rect()
+        restartText_rect.center = restartButton.center
+        screen.blit(restartButtonText, restartText_rect)
+        
+        genFont = pygame.font.Font(pygame.font.get_default_font(), 16)
+        genText = genFont.render(f'Generation: {generation} - FPS: {fps}', True, BLACK)
+        screen.blit(genText, ((screen.get_width() - genText.get_width())/2, (screen.get_height() - 73)))
 
 
-    world = newWorld
+        world = newWorld
+        generation = generation + 1
 
-    # --- Go ahead and update the screen with what we've drawn.
-    pygame.display.flip()
- 
-    # --- Limit to 5 frames per second
-    clock.tick(5)
+        # --- Go ahead and update the screen with what we've drawn.
+        pygame.display.flip()
+        
+
+        # --- Limit to 5 frames per second
+        clock.tick(fps)
+
+    elif state == 'PAUSED':
+        for (x, y), value in np.ndenumerate(currentWorld):
+            if currentWorld[x, y] == 1:
+                pygame.draw.rect(screen, WHITE, (CELL_SIZE * (x - 1), CELL_SIZE * (y - 1), CELL_SIZE, CELL_SIZE), MARGIN)
+                pygame.draw.rect(screen, GREEN, (CELL_SIZE * (x - 1), CELL_SIZE * (y - 1), CELL_SIZE, CELL_SIZE), 0)
+
+            else:
+                pygame.draw.rect(screen, WHITE, (CELL_SIZE * (x - 1), CELL_SIZE * (y - 1), CELL_SIZE, CELL_SIZE), MARGIN)
+                pygame.draw.rect(screen, SILVER, (CELL_SIZE * (x - 1), CELL_SIZE * (y - 1), CELL_SIZE, CELL_SIZE), 0)
+            
+        pygame.draw.rect(screen, GREEN, fasterButton)
+        pygame.draw.rect(screen, GREEN, slowerButton)
+        pygame.draw.rect(screen, GREEN, togglePlayButton)
+        pygame.draw.rect(screen, GREEN, restartButton)
+
+        font = pygame.font.Font(pygame.font.get_default_font(), 20)
+        fasterButtonText = font.render("FASTER", True, BLACK)
+        slowerButtonText = font.render("SLOWER", True, BLACK)
+        togglePlayButtonText = font.render("PLAY/PAUSE", True, BLACK)
+        restartButtonText = font.render("RESTART", True, BLACK)
+
+        fasterText_rect = fasterButtonText.get_rect()
+        fasterText_rect.center = fasterButton.center
+        screen.blit(fasterButtonText, fasterText_rect)
+
+        slowerText_rect = slowerButtonText.get_rect()
+        slowerText_rect.center = slowerButton.center
+        screen.blit(slowerButtonText, slowerText_rect)
+
+        togglePlayText_rect = togglePlayButtonText.get_rect()
+        togglePlayText_rect.center = togglePlayButton.center
+        screen.blit(togglePlayButtonText, togglePlayText_rect)
+
+        restartText_rect = restartButtonText.get_rect()
+        restartText_rect.center = restartButton.center
+        screen.blit(restartButtonText, restartText_rect)
+        
+        genFont = pygame.font.Font(pygame.font.get_default_font(), 16)
+        genText = genFont.render(f'Generation: {generation} - FPS: {fps}', True, BLACK)
+        screen.blit(genText, ((screen.get_width() - genText.get_width())/2, (screen.get_height() - 73)))
+
+        world = currentWorld
+
+
+
  
 # Close the window and quit.
 pygame.quit()
